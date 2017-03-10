f2db10640c6aea9a536f611c92446b1961eec307
Kevin Froman <kevinfroman12@gmail.com>
2017-03-09 19:57:41 -0600

    improved captcha by adding addition problems and switching to better random numbers

--- php/captcha.php
+++ php/captcha.php
@@ -9,8 +9,23 @@ include('settings.php');
 
 // Securely generate captcha
 $captchaBytes = 3;
-$text = bin2hex(openssl_random_pseudo_bytes($captchaBytes));
-$_SESSION['captchaVal'] = $text;
+
+$solve = mt_rand(1, 2);
+$max = 100;
+$num1 = 0;
+$num2 = 0;
+$calcAnswer = 0;
+if ($solve == 1){
+  $num1 = mt_rand(0, $max);
+  $num2 = mt_rand(0, $max);
+  $calcAnswer = $num1 + $num2;
+  $_SESSION['captchaVal'] = strval($calcAnswer);
+  $text = 'Solve: ' . strval($num1) . ' + ' . strval($num2);
+}
+else{
+  $text = bin2hex(openssl_random_pseudo_bytes($captchaBytes));
+  $_SESSION['captchaVal'] = $text;
+}
 
 // Create a blank image and add some text
 $im = imagecreatefromjpeg("captchabg.jpg");
@@ -19,12 +34,17 @@ $text_color = imagecolorallocate($im, 233, 14, 91);
   $linecolor = imagecolorallocate($im, 0xCC, 0xCC, 0xCC);
   // draw random lines on canvas
   for($i=0; $i < 6; $i++) {
-    imagesetthickness($im, rand(1,3));
-    imageline($im, 0, rand(0,30), 120, rand(0,30), $linecolor);
+    imagesetthickness($im, mt_rand(1,3));
+    imageline($im, 0, mt_rand(0,30), 120, mt_rand(0,30), $linecolor);
 
   }
 
-imagestring($im, 5, rand(1, 100), rand(1, 20),  $text, $text_color);
+if ($solve == 1){
+  imagestring($im, 5, 10, 10,  $text, $text_color);
+}
+else{
+imagestring($im, 5, mt_rand(1, 100), mt_rand(1, 20),  $text, $text_color);
+}
 
 // Set the content type header - in this case image/jpeg
 header('Content-Type: image/jpeg');
